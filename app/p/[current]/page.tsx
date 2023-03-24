619841b451d504c1b34f74bba78dccc5365cc738
Kazuki Shibata <shibata@microcms.co.jp>
2023-03-24 16:29:46 +0900

    [update] リストのコンポーネントで共通化

--- app/p/[current]/page.tsx
+++ app/p/[current]/page.tsx
@@ -1,10 +1,7 @@
-import Link from 'next/link';
-import Image from 'next/image';
 import { getList } from '@/libs/microcms';
-import { formatDate } from '@/libs/utils';
 import { LIMIT } from '@/constants';
 import Pagination from '@/components/Pagination';
-import styles from './page.module.css';
+import ListItem from '@/components/ListItem';
 
 type Props = {
   params: {
@@ -22,42 +19,7 @@ export default async function Page({ params }: Props) {
     <div>
       <ul>
         {data.contents.map((article) => (
-          <li key={article.id} className={styles.list}>
-            <Link href={`/articles/${article.id}`} className={styles.link}>
-              {article.thumbnail ? (
-                <picture>
-                  <source
-                    type="image/webp"
-                    media="(max-width: 640px)"
-                    srcSet={`${article.thumbnail?.url}?fm=webp&w=414 1x, ${article.thumbnail?.url}?fm=webp&w=414&dpr=2 2x`}
-                  />
-                  <source
-                    type="image/webp"
-                    srcSet={`${article.thumbnail?.url}?fm=webp&fit=crop&w=240&h=126 1x, ${article.thumbnail?.url}?fm=webp&fit=crop&w=240&h=126&dpr=2 2x`}
-                  />
-                  <img
-                    src={article.thumbnail?.url || `/noimage.png`}
-                    alt=""
-                    className={styles.image}
-                    width={article.thumbnail?.width}
-                    height={article.thumbnail?.height}
-                  />
-                </picture>
-              ) : (
-                <Image
-                  className={styles.image}
-                  src="/no-image.png"
-                  alt="No Image"
-                  width={1200}
-                  height={630}
-                />
-              )}
-              <dl className={styles.content}>
-                <dt className={styles.title}>{article.title}</dt>
-                <dd>{formatDate(article.publishedAt || article.createdAt)}</dd>
-              </dl>
-            </Link>
-          </li>
+          <ListItem key={article.id} article={article} />
         ))}
       </ul>
       <Pagination totalCount={data.totalCount} current={current} />
---
+++ components/ListItem/index.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { formatDate } from '@/libs/utils';
+import { Article } from '@/libs/microcms';
+import styles from './index.module.css';
+
+type Props = {
+  article: Article;
+};
+
+export default function Pagination({ article }: Props) {
+  return (
+    <li className={styles.list}>
+      <Link href={`/articles/${article.id}`} className={styles.link}>
+        {article.thumbnail ? (
+          <picture>
+            <source
+              type="image/webp"
+              media="(max-width: 640px)"
+              srcSet={`${article.thumbnail?.url}?fm=webp&w=414 1x, ${article.thumbnail?.url}?fm=webp&w=414&dpr=2 2x`}
+            />
+            <source
+              type="image/webp"
+              srcSet={`${article.thumbnail?.url}?fm=webp&fit=crop&w=240&h=126 1x, ${article.thumbnail?.url}?fm=webp&fit=crop&w=240&h=126&dpr=2 2x`}
+            />
+            <img
+              src={article.thumbnail?.url || `/noimage.png`}
+              alt=""
+              className={styles.image}
+              width={article.thumbnail?.width}
+              height={article.thumbnail?.height}
+            />
+          </picture>
+        ) : (
+          <Image
+            className={styles.image}
+            src="/no-image.png"
+            alt="No Image"
+            width={1200}
+            height={630}
+          />
+        )}
+        <dl className={styles.content}>
+          <dt className={styles.title}>{article.title}</dt>
+          <dd>{formatDate(article.publishedAt || article.createdAt)}</dd>
+        </dl>
+      </Link>
+    </li>
+  );
+}
