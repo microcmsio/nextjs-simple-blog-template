4114396bf8d59ba48c6d036823facfc0fd814276
Kazuki Shibata <shibata@microcms.co.jp>
2023-04-05 19:31:08 +0900

    [update] 0件時の表示を追加

--- components/ArticleList/index.tsx
+++ components/ArticleList/index.tsx
@@ -9,6 +9,9 @@ export default function ArticleList({ articles }: Props) {
   if (!articles) {
     return null;
   }
+  if (articles.length === 0) {
+    return <p>記事がありません。</p>;
+  }
   return (
     <ul>
       {articles.map((article) => (
