b083e9a9ffd2893a813670971f044111e3791c38
Kazuki Shibata <shibata@microcms.co.jp>
2023-05-26 15:51:26 +0900

    [update] プレビューの実装を変更

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -6,12 +6,17 @@ type Props = {
   params: {
     slug: string;
   };
+  searchParams: {
+    dk: string;
+  };
 };
 
 export const revalidate = 0;
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const data = await getDetail(params.slug);
+export async function generateMetadata({ params, searchParams }: Props): Promise<Metadata> {
+  const data = await getDetail(params.slug, {
+    draftKey: searchParams.dk,
+  });
 
   return {
     title: data.title,
@@ -24,7 +29,10 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-export default async function Page({ params }: Props) {
-  const data = await getDetail(params.slug);
+export default async function Page({ params, searchParams }: Props) {
+  const data = await getDetail(params.slug, {
+    draftKey: searchParams.dk,
+  });
+
   return <Article data={data} />;
 }
