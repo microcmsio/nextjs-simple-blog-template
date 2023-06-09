752526150319f067d610ffe29b3e12768da1abd7
Kazuki Shibata <shibata@microcms.co.jp>
2023-06-09 16:18:56 +0900

    [fix] 存在しないlangを入力された場合のエラー処理

--- libs/utils.ts
+++ libs/utils.ts
@@ -12,11 +12,17 @@ export const formatDate = (date: string) => {
 
 export const formatRichText = (richText: string) => {
   const $ = cheerio.load(richText);
+  const highlight = (text: string, lang?: string) => {
+    if (!lang) return hljs.highlightAuto(text);
+    try {
+      return hljs.highlight(text, { language: lang?.replace(/^language-/, '') || '' });
+    } catch (e) {
+      return hljs.highlightAuto(text);
+    }
+  };
   $('pre code').each((_, elm) => {
     const lang = $(elm).attr('class');
-    const res = lang
-      ? hljs.highlight($(elm).text(), { language: lang?.replace(/^language-/, '') || '' })
-      : hljs.highlightAuto($(elm).text());
+    const res = highlight($(elm).text(), lang);
     $(elm).html(res.value);
   });
   return $.html();
